34c4e36b5abff6119b0295616c5406a2b89d016c
Junjyoti Changmai <junjyotichangmai12@gmail.com>
2024-01-18 12:35:02 +0530

    updating shop section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -6,8 +6,10 @@ const Hero = () => {
   return (
     <div className='hero'>
       <div className="hero-text">
-      <h1>FLAT 40% OFF</h1>
-      <h2>EXTRA 15% OFF ABOVE INR 4499</h2>
+        <div className="message">
+          <h1>FLAT 40% OFF</h1>
+          <h2>EXTRA 15% OFF ABOVE INR 4499</h2>
+        </div>
       </div>
     </div>
   )
